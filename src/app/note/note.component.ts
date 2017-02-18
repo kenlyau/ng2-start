1c009f8c9ca204af785b41fdf64ebe51795744b9
kenlyau <newallah@gmail.com>
2017-02-18 22:00:23 +0800

    fix note.component

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-note',
-  host: {'class': 'app-container'}
+  host: {'class': 'app-container'},
   templateUrl: './note.component.html',
   styleUrls: ['./note.component.css']
 })
